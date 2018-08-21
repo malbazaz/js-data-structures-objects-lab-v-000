fb9fb385b7d68f4bdc7fc1b462839f3a9efff029
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-21 18:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ return driver
 }
 
 function deleteFromDriverByKey(driver,key){
-const newdriver = Object.assign({},driver)
+let newdriver = Object.assign({},driver)
 delete newdriver.key;
 return newdriver;
 }
