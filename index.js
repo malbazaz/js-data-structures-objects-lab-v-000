e008ab27c63e5697839c3ecd252fa90e9ad1256d
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-21 18:34:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver,key,value){
-const newdriver = = {...driver, key:value};
-  return newdriver;
+const newdriver = {...driver, key:value};
+return newdriver;
 }
